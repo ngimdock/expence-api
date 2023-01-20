c6b2241729bb3c858615599433cbae08efce2bd9
ngimdock <ngimdockzemfack@gmail.com>
2023-01-20 23:19:35 +0100

    Adding the database

---
+++ prisma/migrations/20230120221424_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "firstname" TEXT,
+    "lastname" TEXT,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
