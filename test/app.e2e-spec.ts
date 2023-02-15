63db6cf2da9c75de0d136db1d0b925ffd18ac7d4
ngimdock <ngimdockzemfack@gmail.com>
2023-02-15 16:14:33 +0100

    Init the e2e test file

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,78 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { CacheModule, CACHE_MANAGER, INestApplication } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
+import { ExpenseModule } from 'src/expense/expense.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { SchedulerModule } from 'src/scheduler/sheduler.module';
+import { RedisClientOptions } from 'redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AdminGuard, SessionGuard } from 'src/auth/guards';
+import * as redisStore from 'cache-manager-redis-store';
 
-describe('AppController (e2e)', () => {
+describe('App E2E', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        PrismaModule,
+        AuthModule,
+        UserModule,
+        ExpenseModule,
+        ScheduleModule.forRoot(),
+        SchedulerModule,
+        CacheModule.registerAsync<RedisClientOptions>({
+          isGlobal: true,
+
+          useFactory: (configService: ConfigService): any => {
+            return {
+              store: redisStore,
+              url: configService.getOrThrow('REDIS_URL'),
+            };
+          },
+
+          imports: [ConfigModule],
+          inject: [ConfigService],
+        }),
+
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+      ],
+
+      providers: [
+        {
+          provide: APP_GUARD,
+          useClass: SessionGuard,
+        },
+        {
+          provide: APP_GUARD,
+          useClass: AdminGuard,
+        },
+      ],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    // close the connexion to redis
+    const cache = app.get(CACHE_MANAGER);
+
+    const cacheClient = cache.store.getClient();
+
+    await cacheClient.quit();
+
+    app.close();
+  });
+
+  describe('AppModule', () => {
+    it('should be defined', () => {
+      expect(app).toBeDefined();
+    });
   });
 });
